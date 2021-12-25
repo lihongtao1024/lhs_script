b85763cc215e43846f41ff86c9e304ac446a70b0
蓦然 <71164325@qq.com>
2021-12-25 12:21:09 +0800

    *强迫症

--- lhs_code.c
+++ lhs_code.c
@@ -330,10 +330,10 @@ static lhscode_dmpfunction(LHSVM* vm, LHSFunction* func)
 
 int lhscode_dmpcode(LHSVM* vm)
 {
-	printf(";----------------------------------------------------------------------------\n");
+	printf(";------------------------------------------------------------------------------------\n");
 	printf(";lhscript v1.0\n");
 	printf(";programmer: lihong	email: 71164325@qq.com\n");
-	printf(";============================================================================\n\n");
+	printf(";====================================================================================\n\n");
 	
 	for (LHSFrame* frame = vm->mainframe; frame; frame = frame->next)
 	{
